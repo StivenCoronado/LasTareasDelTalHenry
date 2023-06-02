9b8aa6fba86d15b35a145e6e3ad459efca48c7b6
Stiven Coronado <coronadostiven468@gmail.com>
2023-06-02 17:55:04 -0500

    Pendiente

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -84,18 +84,14 @@ function colors(color) {
    switch(color){
       case 'blue':
          return 'This is blue' ;
-         break ;
       case 'red':
          return 'This is red' ;
-         break ;
       case 'green':
          return 'This is green' ;
-         break ;
       case 'orange':
          return 'This is orange';
       default:
          return 'Color not found';
-         break ;
 
    }
 
